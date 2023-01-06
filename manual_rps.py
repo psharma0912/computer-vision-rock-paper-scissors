1739e96eaa38407f43db4de6c3dc7d4accbf3243
psharma0912 <poonam.sharma91212@gmail.com>
2023-01-06 17:18:33 +0000

    adding functions

--- manual_rps.py
+++ manual_rps.py
@@ -1,15 +1,14 @@
 import random
+word_list=['rock', 'paper', 'scissor']
 
 def get_computer_choice():
-    word_list=['rock', 'paper', 'scissor']
-    computer_choice=random.choice(word_list)
-    return computer_choice
+   computer_choice=random.choice(word_list)
+   return computer_choice
 
 def get_user_choice():
-    while True:
-        user_choice=input('Enter a choice:')
-        return user_choice
-    #get_computer_choice()
+    user_choice=input('Enter a choice:')
+    return user_choice
+    
 
 
 
