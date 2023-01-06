dda2f8d59b6fb46040e00a6de5f419dcecf56836
psharma0912 <poonam.sharma91212@gmail.com>
2023-01-06 20:40:30 +0000

    Adding files

--- manual_rps.py
+++ manual_rps.py
@@ -1,34 +1,29 @@
 import random
-choice_list=['rock', 'paper', 'scissors']
-
-def get_computer_choice():
-   computer_choice=random.choice(choice_list)
-   print(computer_choice)
-   return computer_choice.lower()
-
-def get_user_choice():
-    user_choice=input('Enter a choice:').lower()
-    print (user_choice)
-    return user_choice
-
-get_computer_choice()
-
-get_user_choice()
+# choice_list=['rock', 'paper', 'scissors']
 
 # def get_computer_choice():
-#         ## This functions randomly picks a choice for computer
-#         return random.choice(choice_list).lower()
-    
+#    computer_choice=random.choice(choice_list)
+#    print(computer_choice)
+#    return computer_choice.lower()
+
 # def get_user_choice():
-#         ## This functions asks the user for a choice to start the game
-#     user_input = input('Please pick one of Rock Paper or Scissor -> ').lower()
-#     # while user_input not in ['rock', 'paper', 'scissor']:
-#     #     user_input = input('Your choice should be one of Rock Paper or Scissor -> ')
-#     return user_input.lower()
-        
+#     user_choice=input('Enter a choice:').lower()
+#     print (user_choice)
+#     return user_choice
+
+# get_computer_choice()
+
+# get_user_choice()
+
+def get_computer_choice():
+        choice_list = ['Rock','Paper','Scissors']
+        computer_choice = random.choice(choice_list).lower()
+        return computer_choice
 
 
-
-
-
-   
+def get_user_choice():
+    while True:
+        user_choice = input('Please choose Rock, Paper or Scissors:').lower()
+        if user_choice == 'rock' or user_choice == 'scissors' or user_choice == 'paper':
+            return user_choice
+           
