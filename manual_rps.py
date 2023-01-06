d766866d9d99562f4d7c9b61657896aba28b950b
psharma0912 <poonam.sharma91212@gmail.com>
2023-01-06 17:21:54 +0000

    adding function

--- manual_rps.py
+++ manual_rps.py
@@ -1,8 +1,8 @@
 import random
-word_list=['rock', 'paper', 'scissor']
+#word_list=['rock', 'paper', 'scissor']
 
 def get_computer_choice():
-   computer_choice=random.choice(word_list)
+   computer_choice=random.choice(['rock', 'paper', 'scissor'])
    return computer_choice
 
 def get_user_choice():
