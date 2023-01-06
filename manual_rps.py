522d95c96056f1a81b95d27ba44f29b33ae09c4c
psharma0912 <poonam.sharma91212@gmail.com>
2023-01-06 21:58:38 +0000

    changes to func

--- manual_rps.py
+++ manual_rps.py
@@ -1,19 +1,19 @@
 import random
-#choice_list=['rock', 'paper', 'scissors']
 
+
+# computer randomly selects a choice   
 def get_computer_choice():
-    choice_list = ['Rock','Paper','Scissors']
-    computer_choice = random.choice(choice_list)
-    return computer_choice.lower()
+    choices = ["rock","paper","scissors"]
+    computer = random.choice(choices)
+    print(f'computer choice is {computer}')
+    return computer
 
 
-
+# user is asked to input their choice  
 def get_user_choice():
-    while True:
-        user_choice = input('Please choose Rock, Paper or Scissors:').lower()
-        if user_choice == 'rock' or user_choice == 'scissors' or user_choice == 'paper':
-            return user_choice
-        else:
-            print('Please choose one of the options selected')
+    player = input('rock, paper, or scissors?: ').lower()
+    return player
 
-        
+get_computer_choice()
+
+get_user_choice()
