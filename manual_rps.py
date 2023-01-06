9153ba98d37df7ca5ca3578c561845b78d0b33df
psharma0912 <poonam.sharma91212@gmail.com>
2023-01-06 22:08:22 +0000

    changes

--- manual_rps.py
+++ manual_rps.py
@@ -3,17 +3,24 @@ import random
 
 # computer randomly selects a choice   
 def get_computer_choice():
-    choices = ["rock","paper","scissors"]
-    computer = random.choice(choices)
+    choice_list = ['rock','paper','scissors']
+    computer = random.choice(choice_list)
     print(f'computer choice is {computer}')
-    return computer
+    return str(computer)
+    # if guess.isalpha()==True and len(guess)==1:
+    #         print('good guess')
+    #     else:
+    #         print('Invalid letter. Please, enter a single alphabetical character.')
 
 
 # user is asked to input their choice  
 def get_user_choice():
-    player = input('rock, paper, or scissors?: ').lower()
-    return player
+    while True:
+        player = input('rock, paper, or scissors?: ').lower()
+        return player
+
 
 get_computer_choice()
 
 get_user_choice()
+
