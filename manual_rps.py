24907bc3f35c4781700f0727e8842e0a5345a543
psharma0912 <poonam.sharma91212@gmail.com>
2023-01-09 12:06:04 +0000

    Changes to the code

--- manual_rps.py
+++ manual_rps.py
@@ -3,23 +3,22 @@ import random
 
 # computer randomly selects a choice   
 def get_computer_choice():
-    choice_list = ['rock','paper','scissors']
-    computer = random.choice(choice_list)
+    computer = random.choice(['rock','paper','scissors'])
     print(f'computer choice is {computer}')
     return computer
    
 # user is asked to input their choice  
 def get_user_choice():
     while True:
-        player = input('rock, paper, or scissors?: ').lower()
+        player = input('rock, paper, or scissors?: ')
         if player in ['rock','paper','scissors']:
             return player
         else:
             print('Invalid choice. Please try again')
 
 
-get_computer_choice()
+#get_computer_choice()
 
-get_user_choice()
+#get_user_choice()
 
 
