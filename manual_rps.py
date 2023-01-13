d2661cc98b85674c000af76270dec32cfd5508b6
psharma0912 <poonam.sharma91212@gmail.com>
2023-01-13 00:34:03 +0000

    adding choices

--- manual_rps.py
+++ manual_rps.py
@@ -1,17 +1,15 @@
 import random
 
-choice_list=['Rock', 'Paper', 'Scissors']
-
 def get_computer_choice():
-    computer_choice =random.choice(choice_list)
+    computer_choice =random.choice(['Rock', 'Paper', 'Scissors'])
     return computer_choice
 
 def get_user_choice():
     while True:
         user_choice=input('Rock, Paper or Scissors?:')
-        if user_choice in choice_list:
+        if user_choice in ['Rock', 'Paper', 'Scissors']:
             return user_choice
         else:
             print('Invalid choice. Please try again')
-            return user_choice
+            
 
