31ba8dd728c3ec1b8ddc0902fe3f7920015820e1
Poonam Sharma <poonam.sharma91212@gmail.com>
2023-01-25 23:06:57 +0000

    adds get winner

--- manual_rps.py
+++ manual_rps.py
@@ -25,7 +25,7 @@ def get_winner(computer_choice, user_choice):
 # def play():
 #     return (get_computer_choice, get_user_choice, get_winner)
 
-# get_winner(get_computer_choice(), get_user_choice())
+get_winner(get_computer_choice(), get_user_choice())
 
 
 
