3b311c59b30f4171eae0ed25053815c4cea15350
Poonam Sharma <poonam.sharma91212@gmail.com>
2023-01-26 14:04:48 +0000

    adds camera_rps

---
+++ camera_rps.py
@@ -0,0 +1,33 @@
+import cv2
+from keras.models import load_model
+import numpy as np
+model = load_model('keras_model.h5')
+cap = cv2.VideoCapture(0)
+data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
+
+def get_prediction():
+    while True: 
+        ret, frame = cap.read()
+        resized_frame = cv2.resize(frame, (224, 224), interpolation = cv2.INTER_AREA)
+        image_np = np.array(resized_frame)
+        normalized_image = (image_np.astype(np.float32) / 127.0) - 1 # Normalize the image
+        data[0] = normalized_image
+        prediction = model.predict(data) #prediction contains the output of the model
+        #Notice that the prediction is a numpy array with one row and four columns. So first, we need to access the first row, and then get the index of the
+        #highest value in the row. 
+        cv2.imshow('frame', frame)
+        # Press q to close the window
+        print(prediction)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+            
+    # After the loop release the cap object
+    cap.release()
+    # Destroy all the windows
+    cv2.destroyAllWindows()
+
+get_prediction()
+
+
+
+
